2553b4005a2095c31ae74f324cf1de9699113621
Miguel Jesus <miguel.alfonsosanchez9@gmail.com>
2023-06-14 00:13:03 +0200

    sigue fallando

--- src/main/java/com/example/InmoCarlosIII/Entities/Usuario.java
+++ src/main/java/com/example/InmoCarlosIII/Entities/Usuario.java
@@ -7,7 +7,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Entity
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "usuarios")
@@ -28,6 +30,7 @@ public class Usuario {
     private String telefono;
 
 
+
     //Relaciones
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Propiedad> propiedad = new ArrayList<>();
@@ -35,68 +38,5 @@ public class Usuario {
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Contrato> contrato = new ArrayList<>();
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    public String getClave() {
-        return clave;
-    }
-
-    public void setClave(String clave) {
-
-        this.clave = new BCryptPasswordEncoder().encode(clave);
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public List<Propiedad> getPropiedad() {
-        return propiedad;
-    }
-
-    public void setPropiedad(List<Propiedad> propiedad) {
-        this.propiedad = propiedad;
-    }
-
-    public List<Contrato> getContrato() {
-        return contrato;
-    }
-
-    public void setContrato(List<Contrato> contrato) {
-        this.contrato = contrato;
-    }
 }
